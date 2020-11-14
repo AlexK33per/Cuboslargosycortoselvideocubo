4921904e3c3c4eefd8c1f48e0cc188ea0aba1dce
k33per <49404378+AlexK33per@users.noreply.github.com>
2020-11-14 14:09:24 +0100

    Primera subidita

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    bool canjump;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("d"))
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x + 0.1f, gameObject.transform.position.y, gameObject.transform.position.z);
+        }
+        
+        if (Input.GetKey("a"))
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x - 0.1f, gameObject.transform.position.y, gameObject.transform.position.z);
+        }
+
+        if (Input.GetKeyDown("w") && canjump == true)
+        {
+            canjump = false;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector3(0, 100, 0));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "ground")
+        {
+            canjump = true;
+        }
+    }
+}
